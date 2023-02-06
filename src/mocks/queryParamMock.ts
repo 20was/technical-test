318b09f16d9f5ca104c421ab3ac4e786a15e031d
Bishwas Adhikari <20wasadk@gmail.com>
2023-02-07 09:58:44 +1100

    added mock files

---
+++ src/mocks/queryParamMock.ts
@@ -0,0 +1,7 @@
+export const queryParamMock = {
+    search: '?page=2',
+    pathname: '/market',
+    state: {},
+    key: 'some-key',
+    hash: '',
+}
---
+++ src/mocks/snowflakeScoreMock.ts
@@ -0,0 +1,10 @@
+import {SnowflakeScores} from "../models/SnowflakeScores";
+
+export const mockSnowflakeScores: SnowflakeScores = {
+    value: 50,
+    income: 60,
+    health: 70,
+    past: 80,
+    future: 90,
+    total: 89
+};
