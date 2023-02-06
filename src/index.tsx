bd3c8a4fd21e12ce1f28ab6c50c5b940a7154534
Bishwas Adhikari <20wasadk@gmail.com>
2023-02-07 10:56:08 +1100

    updated package file

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import {DEFAULT_MARKET} from "./models/Markets";
+import {API_ENDPOINT} from "./constants/Secrets";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+console.log("ENV:",API_ENDPOINT)
 root.render(
       <Router>
           <Routes>
